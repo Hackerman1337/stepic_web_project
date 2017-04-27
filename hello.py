bc8fcd8c7853c9013b2b148ca46ff7c6609ad18a
Hackerman1337 <bardak1337@live.ru>
2017-04-27 14:57:25 +0300

    Fix hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
-def app(env, start_response):
-	params = [bytes(i + '\r\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
-	body = []
-	status = '200 OK'
-	headers = [('Content-Type', 'text/plain')]
+# def app(env, start_response):
+# 	params = [bytes(i + '\r\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
+# 	body = []
+# 	status = '200 OK'
+# 	headers = [('Content-Type', 'text/plain')]
+# 	start_response(status, headers)
+# 	return body
+
+def app(environ, start_response):
+	params = environ["QUERY_STRING"]
+	body = "\n".join(params.split("&"))
+	status = "200 OK"
+	headers = [
+		("Content-Type", "text/plain"),
+		("Content-Length", str(len(body)))
+	]
 	start_response(status, headers)
-	return body
+	return iter([body])
