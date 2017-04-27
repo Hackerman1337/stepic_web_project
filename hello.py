7ecdabe4e4ff2e243538eaea4afe6c7f07cb8c54
Hackerman1337 <bardak1337@live.ru>
2017-04-27 12:34:32 +0300

    Added wsgi app

---
+++ hello.py
@@ -0,0 +1,7 @@
+def application(env, start_response):
+	params = [bytes(i + '\r\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
+	body = []
+	status = '200 OK'
+	headers = [('Content-Type', 'text/plain')]
+	start_response(status, headers)
+	return body
